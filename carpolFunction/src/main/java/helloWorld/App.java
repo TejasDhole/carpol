b8e8ef5a5337db2d5f2f4618871f6133008c55e7
Tejas <dholetejas039@gmail.com>
2024-04-06 19:04:00 +0530

    Initial commit

---
+++ carpolFunction/src/main/java/helloWorld/App.java
@@ -0,0 +1,116 @@
+package helloWorld;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import helloWorld.userService;
+
+import com.google.gson.Gson;
+
+
+public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    private static final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    private static final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        String httpMethod = input.getHttpMethod();
+        String resourcePath = input.getPath();
+        String output;
+        int statusCode;
+//
+//        switch (httpMethod) {
+//            case "GET":
+//                output = userService.getUsers();
+//                statusCode = 200;
+//                break;
+//            case "POST":
+//                output = userService.createUser(input.getBody());
+//                statusCode = 201;
+//                break;
+//            case "PUT":
+//                output = userService.updateUser(input.getBody());
+//                statusCode = 200;
+//                break;
+//            case "DELETE":
+//                output = userService.deleteUser(input.getBody());
+//                statusCode = 200;
+//                break;
+//            default:
+//                output = "Invalid HTTP Method";
+//                statusCode = 400;
+//                break;
+//        }
+        if (resourcePath.equals("/user") && httpMethod.equals("GET")) {
+            output = userService.getUsers();
+             statusCode = 200;
+            // } else if (resourcePath.equals("/user") && httpMethod.equals("POST")) {
+            //      postUser(event, context);
+             } else if (resourcePath.startsWith("/user/") && httpMethod.equals("GET")) {
+            String userId = resourcePath.substring("/user/".length());
+                  output = userId;
+                    statusCode = 200;
+            // } else if (resourcePath.startsWith("/user/") && httpMethod.equals("PUT")) {
+            //      updateUser(event, context);
+            // } else if (resourcePath.startsWith("/user/") && httpMethod.equals("DELETE")) {
+            //     deleteUser(event, context);
+        } else {
+            output = "Invalid HTTP Method";
+            statusCode = 400;
+        }
+
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        headers.put("X-Custom-Header", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withStatusCode(statusCode)
+                .withHeaders(headers)
+                .withBody(output);
+
+        return response;
+    }
+
+}
+
+// import java.util.HashMap;
+// import java.util.Map;
+
+// import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+// import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+// import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+// import com.amazonaws.services.lambda.runtime.Context;
+// import com.amazonaws.services.lambda.runtime.RequestHandler;
+// import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+// import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+// public class App1 implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+//     public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+
+//         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+//         DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+
+//         Map<String, String> headers = new HashMap<>();
+//         headers.put("Content-Type", "application/json");
+//         headers.put("X-Custom-Header", "application/json");
+//         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent().withHeaders(headers);
+//             String output = String.format("{ \"message\": \"hello world\", \"location\": \"%s\" }", "pageContents");
+
+//             User user = new User();
+//             user.setEmpId("EMP001");
+//             user.setName("John Doe");
+//             user.setEmail("johndoe@example.com");
+
+//             dynamoDBMapper.save(user);
+//             context.getLogger().log("Successfully added new Employee");
+
+//             return response.withStatusCode(200).withBody(output);
+//     }
+// }
---
+++ carpolFunction/src/main/java/helloWorld/userService.java
@@ -0,0 +1,58 @@
+package helloWorld;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.google.gson.Gson;
+
+public class userService {
+    private static final AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().build();
+    private static final DynamoDBMapper dynamoDBMapper = new DynamoDBMapper(client);
+
+    public static String getUsers() {
+        List<User> users = dynamoDBMapper.scan(User.class, new DynamoDBScanExpression());
+        return new Gson().toJson(users);
+    }
+
+    public static String createUser(String requestBody) {
+        User user = new Gson().fromJson(requestBody, User.class);
+        dynamoDBMapper.save(user);
+        return "User created: " + user.getUserId();
+    }
+    public static String updateUser(String requestBody) {
+        User updatedUser = new Gson().fromJson(requestBody, User.class);
+        User existingUser = dynamoDBMapper.load(User.class, updatedUser.getUserId());
+        if (existingUser != null) {
+            existingUser.setName(updatedUser.getName());
+            existingUser.setEmail(updatedUser.getEmail());
+            existingUser.setPhone(updatedUser.getPhone());
+            existingUser.setGender(updatedUser.getGender());
+            existingUser.setDob(updatedUser.getDob());
+            existingUser.setAbout(updatedUser.getAbout());
+            dynamoDBMapper.save(existingUser);
+            return "User updated: " + existingUser.getUserId();
+        } else {
+            return "User not found";
+        }
+    }
+
+
+    public static String deleteUser(String requestBody) {
+        User userToDelete = new Gson().fromJson(requestBody, User.class);
+        User existingUser = dynamoDBMapper.load(User.class, userToDelete.getUserId());
+        if (existingUser != null) {
+            dynamoDBMapper.delete(existingUser);
+            return "User deleted: " + existingUser.getUserId();
+        } else {
+            return "User not found";
+        }
+    }
+}
